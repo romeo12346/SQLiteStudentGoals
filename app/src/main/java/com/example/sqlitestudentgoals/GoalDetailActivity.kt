3c310f9e6cc8ccf9310f6e78618515a039450522
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-09-11 11:44:23 +0200

    Updated .xml files more

--- app/src/main/java/com/example/sqlitestudentgoals/GoalDetailActivity.kt
+++ app/src/main/java/com/example/sqlitestudentgoals/GoalDetailActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sqlitestudentgoals
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class GoalDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goaldetail)
+
+        val txtgoaltitle: TextView = findViewById(R.id.txtgoaltitle)
+        val txtgoaldesc: TextView = findViewById(R.id.txtgoaldesc)
+        val txtgoaldstatus: TextView = findViewById(R.id.txtgoaldstatus)
+        val btnmark: Button = findViewById(R.id.btnaccesssettings)
+
+        btnmark.setOnClickListener(){
+        }
     }
 }
--- app/src/main/java/com/example/sqlitestudentgoals/MainActivity.kt
+++ app/src/main/java/com/example/sqlitestudentgoals/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sqlitestudentgoals
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtgoallist: TextView = findViewById(R.id.txtgoallist)
+        val btnnewgoal: Button = findViewById(R.id.btnnewgoal)
+        val btnaccesssettings: Button = findViewById(R.id.btnaccesssettings)
+
+        btnnewgoal.setOnClickListener(){
+        }
+        btnaccesssettings.setOnClickListener(){
+            val i = Intent(applicationContext, SettingsActivity::class.java)
+            startActivity(i)
+        }
     }
 }
