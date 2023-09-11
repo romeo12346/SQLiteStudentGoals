34a2fdd725e4758e6fa65b2476c34232ff4786bb
FruityLoopes <chadandrews07@gmail.com>
2023-09-11 11:02:35 +0200

    made XML

--- app/src/main/java/com/example/sqlitestudentgoals/GoalDetailActivity.kt
+++ app/src/main/java/com/example/sqlitestudentgoals/GoalDetailActivity.kt
@@ -1,4 +1,11 @@
 package com.example.sqlitestudentgoals
 
-class GoalDetailActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoalDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goaldetail)
+    }
 }
--- app/src/main/java/com/example/sqlitestudentgoals/SettingsActivity.kt
+++ app/src/main/java/com/example/sqlitestudentgoals/SettingsActivity.kt
@@ -1,4 +1,11 @@
 package com.example.sqlitestudentgoals
 
-class SettingsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
 }
